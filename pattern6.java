faab48d81c8652ba7f85f8ef3ddd2fd8584cf939
swaathihaa <swaathihaa@gmail.com>
2025-02-25 22:05:33 +0530

    Update pattern6.java

--- pattern6.java
+++ pattern6.java
@@ -4,13 +4,9 @@ class pattern6{
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n-i+1;j++){
                 System.out.print("*");
-        
             }
             System.out.println();
         }
-
-
-
     }
 }
 
